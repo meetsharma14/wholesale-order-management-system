c796eb52c3d4b616895d27a45adf3d50506721c8
Meet Sharma <meetsharma1569@gmail.com>
2026-07-24 00:27:22 +0530

    Refactor loginUser to use form data for credentials

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,16 @@
 import api from "../api/axios";
 
-export const loginUser = async (data) => {
-  const response = await api.post("/auth/login", data);
+export const loginUser = async ({ email, password }) => {
+  const formData = new URLSearchParams();
+
+  formData.append("username", email);
+  formData.append("password", password);
+
+  const response = await api.post("/auth/login", formData, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
   return response.data;
 };
